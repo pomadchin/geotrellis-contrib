0326c945fd6c10a249b8961dfb0724f9e7ad20de
Grigory Pomadchin <gr.pomadchin@gmail.com>
2019-06-10 17:04:58 -0400

    Update GDALRasterSources up to the new API

--- vlm/src/main/scala/geotrellis/contrib/vlm/geotiff/GeoTiffReprojectRasterSource.scala
+++ vlm/src/main/scala/geotrellis/contrib/vlm/geotiff/GeoTiffReprojectRasterSource.scala
@@ -55,6 +55,7 @@ case class GeoTiffReprojectRasterSource(
       case Some(targetRegion: TargetRegion[Long]) => targetRegion.region
       case Some(targetGrid: TargetGrid[Long]) => targetGrid(reprojectedRasterExtent)
       case Some(dimensions: Dimensions[Long]) => dimensions(reprojectedRasterExtent)
+      case Some(targetCellSize: TargetCellSize[Long]) => targetCellSize(reprojectedRasterExtent)
       case _ => reprojectedRasterExtent
     }
   }
